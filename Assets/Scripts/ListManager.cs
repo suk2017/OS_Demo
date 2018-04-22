6bbdb753bc28bd7225327878750714f38ac58884
suk2017 <sunzuyu2016@foxmail.com>
2018-04-22 16:12:34 +0800

    2018/4/22

--- Assets/Scripts/ListManager.cs
+++ Assets/Scripts/ListManager.cs
@@ -3,23 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ListManager : MonoBehaviour
 {
     public GameObject page_Prefag;
-    public int[] Pages;//初始序列
+    //public int[] Pages;//初始序列
     public Transform[] position;
     public Texture2D[] num;//数字材质
     public Material mat;//材质
-    public int count;//页框个数 用于初始化
-
     public float length;
-    [Range(0, 2)] public int RS;
+    public GameObject setting;
+    public InputField PagesList;
+    public InputField PagePosCount;
+    public Dropdown Dropdown_RS;
 
 
+    [Range(0, 2)] private int RS;
+    private int count;//页框个数 用于初始化
     private bool isOPT = false;//是否是最佳算法
     private GameObject[] lastObject;//变动页框移除的页
     private GameObject[] currentObject;//变动页框移入的页
+    private bool waiting = false;
+    private EventSystem eventSystem;
 
 
     public static bool avaliable = false;//是否可以进行下一步
@@ -39,9 +47,9 @@ public class ListManager : MonoBehaviour
     /// <returns></returns>
     private IEnumerator PageStream()
     {
-        while (used < count)
+        while (step < pages.Count - 1)
         {
-            yield return new WaitUntil(() => avaliable && step < pages.Count - 1);//等到用户按下按钮或者自动下一步
+            yield return new WaitUntil(() => avaliable);//等到用户按下按钮或者自动下一步
 
             if (isOPT)
             {
@@ -68,11 +76,13 @@ public class ListManager : MonoBehaviour
     private void NextPage_OPT()
     {
         ++step;
-        if (step <= 0 || steps[step] != steps[step - 1])
+        print(pages[step] + " " + memory[steps[step]]);
+        if (step <= 0 || pages[step] != memory[steps[step]])
         {
             StartCoroutine(MoveModel());//载入内存中
         }
         ChangeColor();//更新模型
+        memory[steps[step]] = pages[step];
     }
 
     /// <summary>
@@ -161,19 +171,68 @@ public class ListManager : MonoBehaviour
         if (RS_IN) { currentRS.IN(); };
     }
 
+    private List<int> StringToList(string value)
+    {
+        List<int> result = new List<int>();
+        string num = "";
+        for (int i = 0; i < value.Length; ++i)
+        {
+            if (value[i] < '0' || value[i] > '9')
+            {
+                int result_Int;
+                if (int.TryParse(num, out result_Int))
+                {
+                    result.Add(result_Int);
+                    num = "";
+                }
+            }
+            else
+            {
+                num += value[i];
+            }
+        }
+        return result;
+    }
+
     private void Initial()
     {
+        pages = new List<int>();
+
+        string list = "";
+        int firstTime = PlayerPrefs.GetInt("FirstTime", 1);
+        if (firstTime == 1)
+        {
+            list = "6 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 6 0 1";
+            pages = StringToList(list);
+            count = 3;
+            RS = 0;
+
+            PlayerPrefs.SetString("pagesList", list);
+            PlayerPrefs.SetInt("pagePosCount", 3);
+            PlayerPrefs.SetInt("dropDown_RS", 0);
+
+
+            PlayerPrefs.SetInt("FirstTime", 0);
+        }
+        else
+        {
+            list = PlayerPrefs.GetString("pagesList");
+            pages = StringToList(list);
+
+            count = PlayerPrefs.GetInt("pagePosCount");
+            RS = PlayerPrefs.GetInt("dropDown_RS");
+
+        }
+
         memory = new int[count];
+        for (int i = 0; i < memory.Length; ++i) { memory[i] = -1; }
         memoryPageModel = new GameObject[count];
         used = 0;
-        pages = new List<int>();
-        pages.AddRange(Pages);
         step = -1;
-        memory = new[] { -1, -1, -1 };
-        //currentObject = new GameObject[count];
-        //lastObject = new GameObject[count];
 
-
+        PagesList.text = list;
+        PagePosCount.text = count + "";
+        Dropdown_RS.value = RS;
         switch (RS)
         {
             case 0: currentRS = new RS_OPT(); break;
@@ -181,29 +240,63 @@ public class ListManager : MonoBehaviour
             case 2: currentRS = new RS_LRU(count); break;
         }
     }
+
+    public void _Setting()
+    {
+        if (setting.activeInHierarchy)
+        {
+            setting.SetActive(false);
+        }
+        else
+        {
+            setting.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 重启
+    /// </summary>
+    public void _Restart()
+    {
+        string pageList = PagesList.text;
+        string pagePosCount = PagePosCount.text;
+        int dropdown_RS = Dropdown_RS.value;
+        List<int> pageList_Int = new List<int>();
+
+        PlayerPrefs.SetString("pagesList", pageList);
+        PlayerPrefs.SetInt("pagePosCount", int.Parse(pagePosCount));
+        PlayerPrefs.SetInt("dropDown_RS", dropdown_RS);
+
+
+        SceneManager.LoadScene("RS");
+    }
     ///////////////////////////////////
 
     private void Start()
     {
         Initial();
         steps = new List<int>();
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
 
         if (currentRS is RS_OPT)
         {
             isOPT = true;
-            pages.Reverse();
-            currentRS = new RS_LRU(count);
-            for (int i = 0; i < pages.Count; ++i)
-            {
-
-                if (!NextPage())//如果未存在于内存中
-                {
-                    GetPos();
-                }
-                KeepRS();//维护算法状态
-            }
-            steps.Reverse();
+            RS_OPT opt = currentRS as RS_OPT;
+            steps = opt.PreCalculate();
             Initial();
+            //pages.Reverse();
+            //currentRS = new RS_LRU(count);
+            //for (int i = 0; i < pages.Count; ++i)
+            //{
+
+            //    if (!NextPage())//如果未存在于内存中
+            //    {
+            //        GetPos();
+            //    }
+            //    KeepRS();//维护算法状态
+            //}
+            //steps.Reverse();
+            //Initial();
         }
         else
         {
@@ -217,7 +310,7 @@ public class ListManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.RightControl))
         {
             avaliable = true;
         }
--- Assets/Scripts/RS_OPT.cs
+++ Assets/Scripts/RS_OPT.cs
@@ -4,9 +4,124 @@ using UnityEngine;
 
 public class RS_OPT : RS_Base
 {
-    public RS_OPT() :base()
+
+
+    private List<int> pageList;
+    private List<int> steps;
+    private int[] Memory;
+    private int used;
+    private int step;
+
+    public RS_OPT() : base()
     {
         RS_Name = "OPT算法";
     }
 
+    /// <summary>
+    /// 预计算序列
+    /// </summary>
+    public List<int> PreCalculate()
+    {
+        pageList = ListManager.pages;
+        steps = new List<int>();
+        Memory = new int[ListManager.memory.Length];
+        for (int i = 0; i < Memory.Length; ++i)
+        {
+            Memory[i] = -1;
+        }
+        used = 0;
+        step = 0;
+        for (int i = 0; i < pageList.Count; ++i)
+        {
+            step = i;
+            int pos;
+            if (Exist(Memory, pageList[i], out pos))
+            {
+                steps.Add(pos);
+            }
+            else
+            {
+                if (used < Memory.Length)
+                {
+                    Memory[used] = pageList[i];
+                    steps.Add(used);
+                    ++used;
+                }
+                else
+                {
+                    pos = Pos();
+                    Memory[pos] = pageList[i];
+                    steps.Add(pos);
+                }
+            }
+        }
+        return steps;
+    }
+
+    /// <summary>
+    /// 检测是否已经存在于整型数组
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="page"></param>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    private bool Exist(int[] list, int page, out int pos)
+    {
+        string sb = "";
+        //长度应该使用used 但是这样也可以 因为初始时为-1
+        for (int i = 0; i < list.Length; ++i)
+        {
+            sb += list[i] + " ";
+            if (page == list[i])
+            {
+                pos = i;
+                return true;
+            }
+        }
+        Debug.Log(sb);
+        pos = -1;
+        return false;
+    }
+
+    /// <summary>
+    /// 向之后的序列寻找最后出现的元素在内存中的位置
+    /// </summary>
+    /// <returns></returns>
+    private int Pos()
+    {
+        int[] tempMemory = new int[Memory.Length];
+        for (int i = 0; i < Memory.Length; ++i)
+        {
+            tempMemory[i] = Memory[i];
+        }
+        int left = tempMemory.Length;
+        int defaultPos = 0;
+        for (int i = step; i < pageList.Count; ++i)
+        {
+            for (int j = 0; j < tempMemory.Length; ++j)
+            {
+                if (tempMemory[j] == pageList[i])
+                {
+                    if (left == 1)
+                    {
+                        return j;
+                    }
+                    else
+                    {
+                        tempMemory[j] = -1;
+                        --left;
+                    }
+                }
+                else
+                {
+                    if (tempMemory[j] != -1)
+                    {
+                        defaultPos = j;
+                    }
+                }
+
+            }
+        }
+        return defaultPos;
+    }
 }
